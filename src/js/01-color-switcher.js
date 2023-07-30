053552cb8a4f84b4fa67f9f9f2aaa7f316746a1c
ruden4 <ruden4@gmail.com>
2023-07-30 19:23:54 +0300

    First Done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -10,7 +10,10 @@ function startChangingColors () {
     intervalId = setInterval(()=>{
 body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`
     },1000)
+    startBtn.disabled = true;
 };
+
 function stopChangingColors () {
     clearInterval(intervalId);
+    startBtn.disabled = false;
 }
