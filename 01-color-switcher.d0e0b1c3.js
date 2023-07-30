94455d45f828b8e7c2cb3c57932fdf0a65c8fe38
ruden4 <126696327+ruden4@users.noreply.github.com>
2023-07-30 16:14:33 +0000

    Deploying to gh-pages from @ ruden4/goit-js-hw-09@82a854e72ed7a92fb715a27f9f49b086b36d7069 🚀

---
+++ 01-color-switcher.d0e0b1c3.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){intervalId=setInterval((()=>{n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(intervalId)}));
+//# sourceMappingURL=01-color-switcher.d0e0b1c3.js.map
