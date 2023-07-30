82a854e72ed7a92fb715a27f9f49b086b36d7069
ruden4 <ruden4@gmail.com>
2023-07-30 19:13:54 +0300

    FirstDone

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,16 @@
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+startBtn.addEventListener('click',startChangingColors);
+stopBtn.addEventListener('click',stopChangingColors);
+
+function startChangingColors () {
+    intervalId = setInterval(()=>{
+body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`
+    },1000)
+};
+function stopChangingColors () {
+    clearInterval(intervalId);
+}
