1266ac3488632b97115b0967edf80ab8dcacef28
ruden4 <ruden4@gmail.com>
2023-08-03 13:05:14 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,19 +1,19 @@
 
-// const startBtn = document.querySelector('button[data-start]');
-// const stopBtn = document.querySelector('button[data-stop]');
-// const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
-// startBtn.addEventListener('click',startChangingColors);
-// stopBtn.addEventListener('click',stopChangingColors);
+startBtn.addEventListener('click',startChangingColors);
+stopBtn.addEventListener('click',stopChangingColors);
 
-// function startChangingColors () {
-//     intervalId = setInterval(()=>{
-// body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`
-//     },1000)
-//     startBtn.disabled = true;
-// };
+function startChangingColors () {
+    intervalId = setInterval(()=>{
+body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`
+    },1000)
+    startBtn.disabled = true;
+};
 
-// function stopChangingColors () {
-//     clearInterval(intervalId);
-//     // startBtn.disabled = false;
-// }
+function stopChangingColors () {
+    clearInterval(intervalId);
+    // startBtn.disabled = false;
+}
